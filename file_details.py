afdae28c26d53d72038bd3416cf3724041c538f1
carbohydratepro <carbohydratepro@gmail.com>
2023-01-16 04:40:09 +0900

    作成したモデルのメモを追加

--- file_details.py
+++ file_details.py
@@ -80,4 +80,69 @@ PE0201.model
 著者名をラベルに
 dm=1, vector_size=300, min_count=1, epochs=20
 ・精度高い
+
+PE0301.model
+著者名をラベルに
+dm=1, vector_size=300, min_count=1, epochs=20
+宮沢賢治 : 0.0 %
+夏目漱石 : 100.0 %
+太宰治 : 1.1235955056179776 %
+芥川龍之介 : 49.184782608695656 %
+正解率： 28.15040650406504 %
+
+PE0302.model
+著者名をラベルに
+dm=0, vector_size=300, min_count=1, epochs=20
+
+PE0401.model
+著者名をラベルに
+dm=1, vector_size=300, min_count=1, epochs=20
+
+PE0402.model
+著者名をラベルに
+dm=0, vector_size=300, min_count=1, epochs=20
+
+PE501.model
+著者名をラベルに
+dm=1, vector_size=300, min_count=1, epochs=20
+
+PE502.model
+著者名をラベルに
+dm=0, vector_size=300, min_count=1, epochs=20
+
+PE0601.model
+著者名をラベルに
+dm=1, vector_size=300, min_count=1, epochs=20
+宮沢賢治 : 0.0 %
+夏目漱石 : 60.416666666666664 %
+太宰治 : 3.153153153153153 %
+芥川龍之介 : 0.0 %
+正解率： 4.477611940298507 %
+
+PE0602.model
+著者名をラベルに
+dm=0, vector_size=300, min_count=1, epochs=20
+宮沢賢治 : 0.0 %
+夏目漱石 : 18.75 %
+太宰治 : 100.0 %
+芥川龍之介 : 0.0 %
+正解率： 28.73134328358209 %
+
+PE0701.model
+著者名をラベルに
+dm=1, vector_size=300, min_count=1, epochs=20
+宮沢賢治 : 100.0 %
+夏目漱石 : 11.11111111111111 %
+太宰治 : 0.0 %
+芥川龍之介 : 95.90443686006826 %
+正解率： 67.83625730994152 %
+
+PE0702.model
+dm=0, vector_size=300, min_count=1, epochs=20
+宮沢賢治 : 100.0 %
+夏目漱石 : 0.0 %
+太宰治 : 100.0 %
+芥川龍之介 : 100.0 %
+正解率： 75.0 %
+
 '''
